c4589b67394bdcf63698c0b92fe85d1abdc5c4a4
st1vc3 <304027875+st1vc3@users.noreply.github.com>
2026-08-01 13:17:06 +0200

    Add cursebreaker WoW addon manager
    Custom derivation fetching the pre-built Linux binary from GitHub releases with autoPatchelf for NixOS compatibility.

---
+++ pkgs/cursebreaker.nix
@@ -0,0 +1,28 @@
+{ stdenv, fetchurl, autoPatchelfHook, gzip, zlib }:
+
+stdenv.mkDerivation rec {
+  pname = "cursebreaker";
+  version = "4.8.4";
+
+  src = fetchurl {
+    url = "https://github.com/AcidWeb/CurseBreaker/releases/download/v${version}/CurseBreaker-linux.gz";
+    hash = "sha256:1q4hwfm44h188j1w181apiimdfyh1am24zddyiq473idi20fgxdz";
+  };
+
+  nativeBuildInputs = [ autoPatchelfHook ];
+  buildInputs = [ stdenv.cc.cc.lib zlib ];
+
+  dontUnpack = true;
+
+  installPhase = ''
+    mkdir -p $out/bin
+    ${gzip}/bin/gunzip -c $src > $out/bin/cursebreaker
+    chmod +x $out/bin/cursebreaker
+  '';
+
+  meta = {
+    description = "CLI addon manager for World of Warcraft";
+    homepage = "https://github.com/AcidWeb/CurseBreaker";
+    mainProgram = "cursebreaker";
+  };
+}
