3adc140591fcba3029eb33f65e12785d3ea49922
st1vc3 <304027875+st1vc3@users.noreply.github.com>
2026-08-03 13:44:44 +0200

    Manage Neovim plugins declaratively
    Replace runtime lazy.nvim installation state with plugins pinned by the Nix flake. Simplify key mappings and plugin setup for immutable Home Manager deployment.

--- config/nvim/lua/keys.lua
+++ config/nvim/lua/keys.lua
@@ -1,7 +1,9 @@
--- save by pressing Escape
-vim.keymap.set('n', '<Esc>', ':w<CR>', { desc = 'Save' })
--- select all
-vim.keymap.set('n', '<C-a>', 'ggVG', { desc = 'Select All' })
--- pasting over a selection no longer clobbers your clipboard
-vim.cmd([[ xnoremap <expr> p 'pgv"'.v:register.'y' ]])
+-- Save without overriding Escape's normal cancel behavior.
+vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', { desc = 'Save' })
 
+vim.keymap.set('n', '<C-a>', 'ggVG', { desc = 'Select All' })
+
+-- Pasting over a selection no longer clobbers the clipboard.
+vim.keymap.set('x', 'p', function()
+  return 'pgv"' .. vim.v.register .. 'y'
+end, { expr = true, desc = 'Paste without replacing register' })
--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -1,13 +1,8 @@
-return {
-  {
-    'NeogitOrg/neogit',
-    dependencies = { 'nvim-lua/plenary.nvim', 'sindrets/diffview.nvim' },
-    keys = { { '<leader>g', function() require('neogit').open() end, desc = 'Neogit' } },
-  },
-  {
-    'lewis6991/gitsigns.nvim',
-    event = 'BufWinEnter',
-    opts = { current_line_blame = true },  -- who last touched this line
-  },
-}
+require('neogit').setup({})
+require('gitsigns').setup({
+  current_line_blame = true,
+})
 
+vim.keymap.set('n', '<leader>g', function()
+  require('neogit').open()
+end, { desc = 'Neogit' })
