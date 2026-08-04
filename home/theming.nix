459b00944b156d0ab0946a44622f76e66e3d4130
st1vc3 <304027875+st1vc3@users.noreply.github.com>
2026-08-04 19:59:27 +0200

    Use Papirus-Dark icon theme across GTK and Qt apps

--- home/theming.nix
+++ home/theming.nix
@@ -4,6 +4,7 @@
   dconf.settings."org/gnome/desktop/interface" = {
     color-scheme = "prefer-dark";
     gtk-theme = "adw-gtk3-dark";
+    icon-theme = "Papirus-Dark";
   };
 
   gtk = {
@@ -11,6 +12,10 @@
     theme = {
       name = "adw-gtk3-dark";
       package = pkgs.adw-gtk3;
+    };
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
     };
     gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
     gtk4.extraConfig.gtk-application-prefer-dark-theme = true;
@@ -24,4 +29,13 @@
       package = pkgs.adwaita-qt6;
     };
   };
+
+  # Qt apps (Quickshell included) resolve icons through qt6ct, not the GTK or
+  # dconf icon-theme settings above. Without an icon_theme here, qt6ct falls
+  # back to bare hicolor, which is why the launcher showed blurry/generic
+  # icons. Point qt6ct at Papirus-Dark so it matches the GTK side.
+  xdg.configFile."qt6ct/qt6ct.conf".text = ''
+    [Appearance]
+    icon_theme=Papirus-Dark
+  '';
 }
