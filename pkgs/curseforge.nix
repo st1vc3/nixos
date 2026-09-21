2879f47f0a021e639da2fb3203695d31320a12e0
st1vc3 <304027875+st1vc3@users.noreply.github.com>
2026-09-21 11:27:20 +0200

    curseforge: 1.320.0-39237 -> 1.321.1-39714

--- pkgs/curseforge.nix
+++ pkgs/curseforge.nix
@@ -24,11 +24,11 @@
 # (X-AppImage-Version).
 let
   pname = "curseforge";
-  version = "1.320.0-39237";
+  version = "1.321.1-39714";
 
   src = fetchurl {
     url = "https://curseforge.overwolf.com/downloads/curseforge-latest-linux.AppImage";
-    hash = "sha256-ddF+Xz+xKqeMKrK1uDHCvKT29swfX1V7Dt6+/suDAxI=";
+    hash = "sha256-4DQZNlrJGY1gGAyqB74+vhhI9lCDPAEQrayhSX5G0Uc=";
   };
 
   # Unpacked separately so the desktop entry and icons can be installed into
